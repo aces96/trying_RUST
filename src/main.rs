089b99685b35623b88672ae1540953207679d7f3
aces96 <esraidi12@gmail.com>
2022-09-22 14:44:08 +0100

    adding some examples

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,30 @@ use std::cmp::Ordering;
 
 fn main() {
 
+//here is an example of scoop in rust the s variable at the end of curly brackets rust call a function "drop"
+//and it drop s from memory soo in line 16 i can't access the variable s
+    // {
+    //     let mut s = String::from("hello");
 
+    //     s.push_str(", world");
+
+    //     println!("{}",s);
+    // }
+
+    // println!("{}", s);
+
+    
+
+
+}
+
+
+
+fn guessing_game() {
 
     loop {
         // generate a number from the range 1-100
-        let number = rand::thread_rng().gen_range(1..=100);
+        let number = rand::thread_rng().gen_range(1..=10);
         // the "mut" it means this variable is mutable because all variables in rust are immutable
         let mut guess = String::new();
         println!("guess the Number");
@@ -31,7 +50,6 @@ fn main() {
                 },
                 Ordering::Greater => println!("too biig")
                 
-            }
-    }
-
+            };
+    };
 }
