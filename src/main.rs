21723eda167bf4bdb83f6e7c4a3bc995d1981b61
aces96 <esraidi12@gmail.com>
2022-09-22 10:38:39 +0100

    my first rust code the guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
 
-    println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        // generate a number from the range 1-100
+        let number = rand::thread_rng().gen_range(1..=100);
+        // the "mut" it means this variable is mutable because all variables in rust are immutable
+        let mut guess = String::new();
+        println!("guess the Number");
+        println!("please input a number");
+
+        //read input 
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read the line");
+
+        //here we parse the String to Number soo we could compare it with the number generated
+        let guess: u32 = guess.trim().parse().expect("please provide number");
+
+    // here we compare the number generated and the number provided
+            match guess.cmp(&number) {
+                Ordering::Less => println!("too small"),
+                Ordering::Equal => {
+                    println!("is equal");
+                    break;
+                },
+                Ordering::Greater => println!("too biig")
+                
+            }
     }
+
 }
